bbb46e9c7617f4b32d44bd99ea30c1323aecfcc8
LibertyReman <libertyreman@gmail.com>
2020-12-29 20:54:51 +0900

    maxcdn.bootstrapcdn.com CDNからテーマ内でのバンドルに変更

--- functions.php
+++ functions.php
@@ -21,9 +21,10 @@
     add_filter( 'pre_get_document_title', 'wpbeg_title' ); //自作関数
 
     function wpbeg_script() {
+        wp_enqueue_style( 'font-awesome', get_template_directory_uri() . '/css/font-awesome.css', array(), '4.7.0' );
         wp_enqueue_style( 'mplus1p', '//fonts.googleapis.com/earlyaccess/mplus1p.css', array() );
         wp_enqueue_style( 'Sacramento', '//fonts.googleapis.com/css?family=Sacramento&amp;amp;subset=latin-ext', array() );
-        wp_enqueue_style( 'font-awesome', '//maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css', array(), '4.7.0' );
+        wp_enqueue_style( 'style', get_template_directory_uri() . '/style.css', array(), '1.0.0' );
         wp_enqueue_style( 'normalize', get_template_directory_uri() . '/css/normalize.css', array(), '4.5.0' );
         wp_enqueue_style( 'wpbeg', get_template_directory_uri() . '/css/wpbeg.css', array(), '1.0.0' );
         wp_enqueue_style( 'style', get_template_directory_uri() . '/style.css', array(), '1.0.0' );
