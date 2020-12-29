24d6bf6f8c3b29a282bc86e3658a07234eeb5f85
LibertyReman <libertyreman@gmail.com>
2020-12-29 21:40:30 +0900

    header.php: get_template_directory_uriにesc_urlを追加

--- header.php
+++ header.php
@@ -7,7 +7,7 @@
         <meta name="format-detection" content="telephone=no">
         <meta name="description" content="WordPress theme development static data for beginners">
         <meta name="keywords" content="WordPress, Theme, development">
-        <link rel="shortcut icon" href="<?php echo get_template_directory_uri(); ?>/images/common/ico/favicon.ico">
+        <link rel="shortcut icon" href="<?php echo esc_url(get_template_directory_uri() ); ?>/images/common/ico/favicon.ico">
         <?php if ( is_singular() ) wp_enqueue_script( "comment-reply" ); ?>
         <?php wp_head(); ?>
     </head>
